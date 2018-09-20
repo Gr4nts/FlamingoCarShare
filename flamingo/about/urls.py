8554d95a73ea83fecc0ff2ac993f8963ab6473aa
Grace <41749221+s3515687@users.noreply.github.com>
2018-09-20 15:15:09 +1000

    removed howtouse page

--- flamingo/about/urls.py
+++ flamingo/about/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('howtouse/', views.HowToUse.as_view(), name='howtouse'),
     path('prices/', views.Prices.as_view(), name='prices'),
     path('about/', views.About.as_view(), name='about'),
     #homepage
