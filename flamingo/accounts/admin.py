b8b6cc595b8a02026d429c4e47f8c9400a61e5db
s3515687 <41749221+s3515687@users.noreply.github.com>
2018-09-27 16:00:12 +1000

    update

--- flamingo/accounts/admin.py
+++ flamingo/accounts/admin.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
 from .models import Car
 from .models import Booking
-#from .models import Available
 
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ['car_name', 'description', 'price'] #, 'available']
-    list_filter = ['price'] #, 'available']
+    list_display = ['car_name', 'description', 'price']
+    list_filter = ['price']
 
 class BookingModelAdmin(admin.ModelAdmin):
     list_display = ['customer', 'car', 'book_start_date', 'book_end_date']
     list_filter = ['customer', 'car']
     date_hierarchy = 'book_start_date'
 
-#class AvailableModelAdmin(admin.ModelAdmin):
-    #list_display = ['avail_id', 'available']
-
 admin.site.register(Car,CarModelAdmin)
 admin.site.register(Booking,BookingModelAdmin)
-#admin.site.register(Available,AvailableModelAdmin)
