9319c02fdf34130b405c216c2996e3d2f7991d37
s3515687 <41749221+s3515687@users.noreply.github.com>
2018-10-12 15:01:04 +1100

    updated booking page

--- flamingo/accounts/views.py
+++ flamingo/accounts/views.py
@@ -11,7 +11,7 @@ from .forms import BookingForm
 from .forms import CustomUserCreationForm
 from .models import CustomUser
 
-class SignUp(SuccessMessageMixin,generic.CreateView):
+class SignUp(generic.CreateView,SuccessMessageMixin):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'signup.html'
@@ -22,7 +22,10 @@ def Account(request):
     context = { 'Bookings': Bookings }
     return render(request, 'account.html', context)
 
-def CreateBooking(request):
+def CreateBooking(request, pk):
+#def CreateBooking(request):
+    car = Car.objects.get(pk=pk)
+
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
@@ -33,15 +36,40 @@ def CreateBooking(request):
             return redirect('bookdone')
     else:
         form = BookingForm(request.POST)
-    return render(request, 'book.html', {'form': form})
+    return render(request, 'book.html', {'form': form, 'car' : car})
 
 class BookDone(generic.TemplateView):
     template_name = 'bookdone.html'
 
+class DeleteBooking(generic.DeleteView,SuccessMessageMixin):
+    model = Booking
+    template_name = 'deletebook.html'
+    success_url = reverse_lazy('account')
+    success_message = "Booking has been deleted."
+    #Bookings = Booking.objects.filter(customer=request.user)
+
+    """def get_queryset(self):
+        return Booking.objects.filter(customer=request.user)"""
+
+    """def delete(self, request, *args, **kwargs):
+       self.object = self.get_object()
+       if self.object.customer == request.user:
+          self.object.delete()
+          return redirect('bookdone')
+      else:
+          raise Http404"""
+
+    """def delete(self, request, *args, **kwargs):
+        if self.object.customer = request.user:
+            self.object.delete()
+        #success_url = self.get_success_url()
+        self.object.delete()
+        return redirect('bookdone')"""
+
 class CarView(generic.DetailView):
-    template_name = 'car_details.html'
     model = Car
+    template_name = 'car_details.html'
 
 class BookingView(generic.DetailView):
-    template_name = 'booking_details.html'
     model = Booking
+    template_name = 'booking_details.html'
